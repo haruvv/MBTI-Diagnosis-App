0515b663ef77b513128357ecbf3314e6cfc88f86
haruvv <haruki.ito0044@gmail.com>
2024-08-20 02:51:14 +0900

    修正

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return 'Hello, World! (root directory!!)';
+    return 'Hello, World! (root directory!!!!)';
 });
 
 Route::get('/test', function () {
