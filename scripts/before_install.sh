a8db8e589d99481a7d697bc124e333e49d4a4708
haruvv <haruki.ito0044@gmail.com>
2024-08-20 02:26:22 +0900

    修正

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,6 +4,9 @@
 #   sudo mv /var/www/MBTI-Diagnosis-App /var/www/MBTI-Diagnosis-App_backup_$(date +%Y%m%d_%H%M%S)
 # fi
 
+rm -rf /var/www/MBTI-Diagnosis-App/.editorconfig
+rm -rf /var/www/MBTI-Diagnosis-App/*
+
 # デプロイ先ディレクトリの作成
 # sudo mkdir -p /var/www/MBTI-Diagnosis-App
 # sudo chown -R ec2-user:ec2-user /var/www/MBTI-Diagnosis-App
