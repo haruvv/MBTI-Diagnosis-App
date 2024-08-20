0ee184694e31f953ff439185ac1bb113068fe4ca
haruvv <haruki.ito0044@gmail.com>
2024-08-20 14:41:31 +0900

    設定

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,13 @@ Route::get('/test', function () {
     return 'Hello, World!!!!!';
 });
 
+Route::get('/env-test', function () {
+    return response()->json([
+        'APP_NAME' => env('APP_NAME'),
+        'APP_KEY' => env('APP_ENV'),
+        'APP_ENV' => env('APP_ENV'),
+        'DB_HOST' => env('DB_HOST'),
+        'DB_DATABASE' => env('DB_DATABASE'),
+    ]);
+});
+
